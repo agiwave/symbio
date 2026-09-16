8caa127c3b02ca6dc706f31d83a7c3f4c966c0ce
billwu <sanbgi@qq.com>
2026-09-16 20:18:04 +0800

    feat(vdfs): add root_hidden implementation for WorkPlugin
    实现VdfsProvider的root_hidden方法，标记工作插件的根目录为隐藏

--- symbio/src/plugins/work/vdfs.rs
+++ symbio/src/plugins/work/vdfs.rs
@@ -82,6 +82,10 @@ impl VdfsProvider for WorkPlugin {
         VdfsAccess::LIST_TRAVERSE
     }
 
+    fn root_hidden(&self) -> bool {
+        true
+    }
+
     /// 根下不可新建：记忆文件是**唯一且恒在**的那一个，没有第二种东西可建
     fn root_new_types(&self) -> Vec<crate::symbio_core::vdfs_provider::VdfsNewType> {
         Vec::new()
